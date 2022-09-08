73dd5ef7e6d14027b773974029d970eabd65389f
Warmick91 <mickiewicz.dbass@gmail.com>
2022-09-08 13:26:20 +0200

    1) Example 2-2

--- src/TransactionAnalyzer/BankTransactionAnalyzerParticularMonths.java
+++ src/TransactionAnalyzer/BankTransactionAnalyzerParticularMonths.java
@@ -4,29 +4,22 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-public class BankTransactionAnalyzerSimple {
+public class BankTransactionAnalyzerParticularMonths {
 	private static final String RESOURCES = "src/main/resources";
 
 	public static void main(String[] args) throws IOException {
-
+		
 		final Path path = Paths.get(RESOURCES + args[0]);
 		final List<String> lines = Files.readAllLines(path);
 		double total = 0d;
-		final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-		for (final String line : lines) {
+		for(final String line: lines) {
 			final String[] columns = line.split(",");
-			final LocalDate date = LocalDate.parse(columns[0]);
-			if (date.getMonth() == Month.JANUARY) {
-				final double amount = Double.parseDouble(columns[1]);
-				total += amount;
-			}
+			final double amount = Double.parseDouble(columns[1]);
+			total += amount;
 		}
-
+		
 		System.out.println("The total for all transactions is " + total);
 	}
 
